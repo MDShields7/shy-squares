2ee1a107f6cdba3d642e0f07d2603ef2527ed696
MDShields7 <matthew.duncan.shields@gmail.com>
2020-09-02 03:02:34 -0500

    prestart muted non-functional game area shown, new square types added, border radius in progress

--- src/components/layout/Level.js
+++ src/components/layout/Level.js
@@ -20,161 +20,57 @@ function Level ({ children }){
     const [gameFault, setGameFault] = useState(false);
     const [gameWin, setGameWin] = useState(false);
     const [gameLose, setGameLose] = useState(false);
-    // const [ botBtn, setBotBtn] = useState(<button onClick={() => setTutorial(false)}>Next</button>)
     const hrefAgain='/level/'+level;
     const hrefNext='/level/'+(level+1)
-    // let gamebox;
-    // const totalSquares = mapOrig.size;
-    // const totalSquaresAcross = Math.sqrt(totalSquares);
-    // const squareWidth = 600 / totalSquaresAcross
-    // const borderWidth = 6;
-    // let hoverMap = new Map();
-    // let clickMap = new Map();
-    // let hover = [];
-    // let click = [];
 
     const totalSquares = mapOrig.size;
     const totalSquaresAcross = Math.sqrt(totalSquares);
     const squareWidth = 600 / totalSquaresAcross;
     let gamebox;
-    // let smRadius = 5+'px';
-    // let mdRadius = (squareWidth/4)+'px';
-    // let lgRadius = (squareWidth/2)+'px';
-    // let green = '#65B540';
-    // let orange = '#EF8A17';
-    // let red = '#DB162F';
+    let progressBtn;
     if ( tutorial ) {
         gamebox = <img src="/shokiri-sumo.gif" />;
-        // console.log('gamebox', gamebox)
+        progressBtn = <button className='button' onClick={() => setTutorial(false)}>Next</button>;
+    } else if ( !tutorial && !gameStart ){
+        const sqArrResults = makeSquareArr( 'pre-start', squareWidth, mapOrig, checkGameMap );
+        [ gamebox ] = sqArrResults;
+        progressBtn = <button className='button' onClick={() => setGameStart(true)}>Start</button>
     } else if ( !tutorial && !gameWin ){
-        [ gamebox ] = makeSquareArr( squareWidth, mapOrig );
-        const [ hoverMap , clickMap ] = makeSquareArr( squareWidth, mapOrig );
-        console.log('gamebox',gamebox)
-        // mapOrig.forEach( function (value, key) {
-        //     // console.log('gamebox - Key:'+key+', Value:'+value)
-        //     let val1 = key[0];
-        //     let val2 = key[1];
-        //     switch ( value ) {
-        //         case 'shy':
-        //             hover = ['end', 'flip'];
-        //             click = ['end', 'na'];
-        //             hoverMap.set(key, 1);
-        //             break;
-        //         case 'bold':
-        //             hover = ['na', 'na'];
-        //             click = ['end', 'demo'];
-        //             clickMap.set(key, 1);
-        //             break;
-        //         case 'calm':
-        //             hover = ['done'];
-        //             click = ['done'];
-        //             break;
-        //         default:
-        //             hover = ['done'];
-        //             click = ['done'];
-        //     }
-        //     let color = green;
-        //     let radius = lgRadius;
-        //     if ( click.length > 1 && click[2] === 'demo' ){
-        //         radius = smRadius;
-        //     } else if ( click.length > 1 && click[1] === 'demo' ){
-        //         radius = mdRadius; 
-        //     }
-        //     if ( hover.length > 1 && hover[2] === 'flip' ) {
-        //         color = red;
-        //     } else if ( hover.length > 1 && hover[1] === 'flip' ) {
-        //         color = orange;
-        //     }
-        //     // console.log('Item'+count+', hover', hover)
-        //     // console.log('Item'+count+', click', click)
-        //     let style = {
-        //         width: (squareWidth - borderWidth)+'px',
-        //         height: (squareWidth - borderWidth)+'px',
-        //         background: color,
-        //         borderRadius: radius,
-        //         border: `${borderWidth/2}px solid #3E1429`,
-        //     }
-        //     squaresArr.push(<div style={style} data={key} key={count} onMouseEnter={(e) => onHover(e, key)} onClick={(e) => onClick(e, key)}></div>)
-        //     count += 1;
-        // })
-        // gamebox = squaresArr;
-        // console.log('gamebox', gamebox)
+        const sqArrResults = makeSquareArr( 'start', squareWidth, mapOrig, checkGameMap );
+        [ gamebox ] = sqArrResults;
+        console.log('hi', gamebox)
+        const [ hoverMap , mapClick ] = sqArrResults;
+        progressBtn = <div>nothing to show now</div>
     } else if ( gameWin === true ){
         gamebox = <div>You win!</div>
+        progressBtn = <button className='button' ><Link href={hrefNext}><a >Next level</a></Link></button>
     } else if ( gameFault || gameLose ){
         gamebox = <div>You lost</div>
+        progressBtn = <button className='button' ><Link href={hrefAgain}><a >Try Again</a></Link></button>
     }
-    
-    // function onHover ( e, arr ) {
-    //     console.log('Hovering! e.target', e.target)
-    //     let sqColor = e.target.style.background;
-    //     if ( sqColor === red) { //red
-    //         e.target.style.background = orange;
-    //     } else {
-    //         e.target.style.background = green;   
-    //     }
-    //     changeGameMap( 'hover', arr )
-    // }
-    // function onClick ( e, arr ) {
-    //     // console.log('e.target', e.target)
-    //     let sqRadius = e.target.style.borderRadius;
-    //     if ( sqRadius === smRadius ) { //red
-    //         e.target.style.borderRadius = mdRadius;
-    //     } else {
-    //         e.target.style.borderRadius = lgRadius;;   
-    //     }
-    //     changeGameMap( 'click', arr )
-    // }
-    // function changeGameMap ( type, mapKey ) {
-    //     // modify scoring maps
-    //     if ( type === 'hover' ){
-    //         let hoverNum = hoverMap.get(mapKey)
-    //         if ( hoverNum > 1 ){
-    //             hoverMap.set( hoverNum - 1 );
-    //         } else {
-    //             hoverMap.delete(mapKey);
-    //         }
-    //     } else if ( type === 'click' ){
-    //         let clickNum = clickMap.get(mapKey)
-    //         if ( clickNum > 1 ){
-    //             clickMap.set( clickNum - 1 );
-    //         } else {
-    //             clickMap.delete(mapKey);
-    //         }
-    //     }
-    //     checkGameMap()
-    // }
-    // function checkGameMap () {
-    //     // check scoring arrays for game over
-    //     if ( hoverMap.size === 0 && clickMap.size === 0 ){
-    //         setGameWin(true);
-    //         console.log('you win')
-    //     }
-    //     console.log('hoverMap.size', hoverMap.size)
-    //     console.log('clickMap.size', clickMap.size)
-    // }
 
-    console.log('tutorial:',tutorial, ', gameStart:',gameStart, ', gameWin', gameWin)
+    function checkGameMap (mapHover, mapClick) {
+        // check scoring arrays for game over
+        if ( mapHover.size === 0 && mapClick.size === 0 ){
+            setGameWin(true);
+            console.log('you win')
+        }
+        console.log('mapHover.size', mapHover.size)
+        console.log('mapClick.size', mapClick.size)
+    }
+
+    console.log('gamebox',gamebox)
+    // console.log('tutorial:',tutorial, ', gameStart:',gameStart, ', gameWin', gameWin)
     return (
         <Site>
             <GameBarTop>
                 {[level, timer, par]}
             </GameBarTop>
-            {/* { children[1] } */}
             <div className={levelcss.gameContainer}>
                 {gamebox}
             </div>
             <div className={`${levelcss.row} ${levelcss.level}`}>
-                {/* { button } */}
-                { tutorial ? 
-                    <button className='button' onClick={() => setTutorial(false)}>Next</button>:
-                !tutorial && gameStart === false ?
-                    <button className='button' onClick={() => setGameStart(true)}>Start</button> :
-                gameStart && gameWin ?
-                    <button className='button' ><Link href={hrefNext}><a >Next level</a></Link></button> :
-                gameLose || gameFault ?
-                    <button className='button' ><Link href={hrefAgain}><a >Try Again</a></Link></button>:
-                    <div>nothing to show now</div> }
+                { progressBtn }
             </div>
         </Site>
     )
--- src/components/lib/makeSquareArr.js
+++ src/components/lib/makeSquareArr.js
@@ -1,34 +1,89 @@
+import chroma from 'chroma-js';
 
-export default function makeSquareArr ( widthOfSq, inputMap ) {
-    let hoverMap = new Map();
-    let clickMap = new Map();
+let hoverMap = new Map();
+let clickMap = new Map();
+
+export default function makeSquareArr ( startType, widthOfSq, inputMap, gameChecker ) {
     let squaresArr = [];
     let hover = [];
     let click = [];
     let count = 0;
-    const borderWidth = 6;
     const borderRadius  = {
         small: 5+'px',
         medium: (widthOfSq/4)+'px',
         large: (widthOfSq/2)+'px'
     }
     const background = {
-        green: '#65B540',
-        orange: '#EF8A17',
-        red: '#DB162F'
+        green: 'rgb(101, 181, 64)',
+        orange: 'rgb(239, 138, 23)',
+        red: 'rgb(219, 22, 47)'
     }
+    function onHover ( e, arr ) {
+        console.log('Hovering! e.target', e.target)
+        let sqColor = e.target.style.background;
+        if ( sqColor === background.red ) { //red
+            e.target.style.background = background.orange;
+        } else if (sqColor === background.orange ){
+            e.target.style.background = background.green;   
+        }
+        changeGameMap( 'hover', arr )
+    }
+    function onClick ( e, arr ) {
+        // console.log('e.target', e.target)
+        let sqRadius = e.target.style.borderRadius;
+        if ( sqRadius === borderRadius.small ) {
+            e.target.style.borderRadius = borderRadius.medium;
+        } else if ( sqRadius === borderRadius.medium ){
+            e.target.style.borderRadius = borderRadius.large;   
+        }
+        changeGameMap( 'click', arr )
+    }
+    function changeGameMap ( type, mapKey ) {
+        // modify scoring maps
+        if ( type === 'hover' ){
+            let hoverNum = hoverMap.get(mapKey)
+            if ( hoverNum > 1 ){
+                hoverMap.set( hoverNum - 1 );
+            } else {
+                hoverMap.delete(mapKey);
+            }
+        } else if ( type === 'click' ){
+            let clickNum = clickMap.get(mapKey)
+            if ( clickNum > 1 ){
+                clickMap.set( clickNum - 1 );
+            } else {
+                clickMap.delete(mapKey);
+            }
+        }
+        gameChecker(hoverMap, clickMap)
+    }
+
     inputMap.forEach( function (value, key) {
         // console.log('gamebox - Key:'+key+', Value:'+value)
         switch ( value ) {
+            case 'shy-2x':
+                hover = ['end', 'flip', 'flip'];
+                click = ['end', 'na', 'na'];
+                hoverMap.set(key, 2);
+                break;
             case 'shy':
                 hover = ['end', 'flip'];
                 click = ['end', 'na'];
                 hoverMap.set(key, 1);
                 break;
+            case 'bold-2x':
+                hover = ['na', 'na', 'na'];
+                click = ['end', 'demo', 'demo'];
+                clickMap.set(key, 2);
+                break;
             case 'bold':
                 hover = ['na', 'na'];
                 click = ['end', 'demo'];
                 clickMap.set(key, 1);
+                break;
+            case 'lava':
+                hover = ['lava'];
+                click = ['lava'];
                 break;
             case 'calm':
                 hover = ['done'];
@@ -38,9 +93,12 @@ export default function makeSquareArr ( widthOfSq, inputMap ) {
                 hover = ['done'];
                 click = ['done'];
         }
+        let borderWidth = 6;
+        const dark2 = 'rgb(62, 20, 41)';
         let color = background.green;
         // console.log('color', color);
         let radius = borderRadius.large;
+        let border = dark2;
         // console.log('radius', radius);
         if ( click.length > 1 && click[2] === 'demo' ){
             radius = borderRadius.small;
@@ -52,68 +110,39 @@ export default function makeSquareArr ( widthOfSq, inputMap ) {
         } else if ( hover.length > 1 && hover[1] === 'flip' ) {
             color = background.orange;
         }
+        if ( hover.length === 1 && hover[0] === 'lava' ){
+            border = background.red;
+            color = dark2;
+            radius = borderRadius.small;
+            borderWidth = 24
+        }
         // console.log('Item'+count+', hover', hover)
         // console.log('Item'+count+', click', click)
-        let style = {
+        let startStyle = {
+            boxSizing: 'border-box',
             width: (widthOfSq - borderWidth)+'px',
             height: (widthOfSq - borderWidth)+'px',
             background: color,
             borderRadius: radius,
-            border: `${borderWidth/2}px solid #3E1429`,
+            border: `${borderWidth/2}px solid ${border}`,
         }
-        squaresArr.push(<div style={style} data={key} key={count} onMouseEnter={(e) => onHover(e, key)} onClick={(e) => onClick(e, key)}></div>)
+        let preStartStyle = {
+            boxSizing: 'border-box',
+            width: (widthOfSq - borderWidth)+'px',
+            height: (widthOfSq - borderWidth)+'px',
+            background: chroma(color).desaturate(2),
+            borderRadius: radius,
+            border: `${borderWidth/2}px solid ${border}`,
+        }
+        if ( startType === 'start' ){
+            squaresArr.push(<div style={startStyle} data={key} key={count} onMouseEnter={(e) => onHover(e, key)} onClick={(e) => onClick(e, key)}></div>)
+        } else if ( startType === 'pre-start' ){
+            squaresArr.push(<div style={preStartStyle} data={key} key={count} ></div>)
+        }
         count += 1;
     })
     // console.log('squaresArr',squaresArr)
     const gamebox = [squaresArr, hoverMap, clickMap];
     return gamebox; 
-}   
+}
 
-function onHover ( e, arr ) {
-    console.log('Hovering! e.target', e.target)
-    let sqColor = e.target.style.background;
-    if ( sqColor === red) { //red
-        e.target.style.background = orange;
-    } else {
-        e.target.style.background = green;   
-    }
-    changeGameMap( 'hover', arr )
-}
-function onClick ( e, arr ) {
-    // console.log('e.target', e.target)
-    let sqRadius = e.target.style.borderRadius;
-    if ( sqRadius === smRadius ) { //red
-        e.target.style.borderRadius = mdRadius;
-    } else {
-        e.target.style.borderRadius = lgRadius;;   
-    }
-    changeGameMap( 'click', arr )
-}
-function changeGameMap ( type, mapKey ) {
-    // modify scoring maps
-    if ( type === 'hover' ){
-        let hoverNum = hoverMap.get(mapKey)
-        if ( hoverNum > 1 ){
-            hoverMap.set( hoverNum - 1 );
-        } else {
-            hoverMap.delete(mapKey);
-        }
-    } else if ( type === 'click' ){
-        let clickNum = clickMap.get(mapKey)
-        if ( clickNum > 1 ){
-            clickMap.set( clickNum - 1 );
-        } else {
-            clickMap.delete(mapKey);
-        }
-    }
-    checkGameMap()
-}
-function checkGameMap () {
-    // check scoring arrays for game over
-    if ( hoverMap.size === 0 && clickMap.size === 0 ){
-        setGameWin(true);
-        console.log('you win')
-    }
-    console.log('hoverMap.size', hoverMap.size)
-    console.log('clickMap.size', clickMap.size)
-}
