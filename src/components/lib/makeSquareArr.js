07a7926fbd7378e4f13bec287e09dbf6d4e1fc68
MDShields7 <matthew.duncan.shields@gmail.com>
2020-09-09 18:07:07 -0500

    this should have been added to the last commit, main changes here, previous commit adds new square types for testing level 1

--- src/components/lib/makeSquareArr.js
+++ src/components/lib/makeSquareArr.js
@@ -1,6 +1,9 @@
 import chroma from 'chroma-js';
 
 let hoverMap = new Map();
+// Each item in hoverMap is composed of:
+// A key, which is the key of the item ( 0 for first square, count+1 for each square)
+// A count, which tells how many times more this can be hovered over
 let clickMap = new Map();
 
 export default function makeSquareArr ( startType, widthOfSq, inputMap, gameChecker ) {
@@ -23,8 +26,10 @@ export default function makeSquareArr ( startType, widthOfSq, inputMap, gameChec
         let sqColor = e.target.style.background;
         if ( sqColor === background.red ) { //red
             e.target.style.background = background.orange;
+            e.target.style.border = background.orange;
         } else if (sqColor === background.orange ){
             e.target.style.background = background.green;   
+            e.target.style.border = background.green;   
         }
         changeGameMap( 'hover', arr )
     }
@@ -40,17 +45,19 @@ export default function makeSquareArr ( startType, widthOfSq, inputMap, gameChec
     }
     function changeGameMap ( type, mapKey ) {
         // modify scoring maps
+        console.log( 'changeGameMap, type:', type, ', mapKey:', mapKey)
         if ( type === 'hover' ){
             let hoverNum = hoverMap.get(mapKey)
+            console.log('hoverNum:', hoverNum)
             if ( hoverNum > 1 ){
-                hoverMap.set( hoverNum - 1 );
+                hoverMap.set( mapKey, hoverNum - 1 );
             } else {
                 hoverMap.delete(mapKey);
             }
         } else if ( type === 'click' ){
             let clickNum = clickMap.get(mapKey)
             if ( clickNum > 1 ){
-                clickMap.set( clickNum - 1 );
+                clickMap.set( mapKey, clickNum - 1 );
             } else {
                 clickMap.delete(mapKey);
             }
@@ -98,23 +105,24 @@ export default function makeSquareArr ( startType, widthOfSq, inputMap, gameChec
         let color = background.green;
         // console.log('color', color);
         let radius = borderRadius.large;
-        let border = dark2;
+        let border = background.green;
         // console.log('radius', radius);
         if ( click.length > 1 && click[2] === 'demo' ){
             radius = borderRadius.small;
         } else if ( click.length > 1 && click[1] === 'demo' ){
             radius = borderRadius.medium; 
-        }
+        }    
         if ( hover.length > 1 && hover[2] === 'flip' ) {
             color = background.red;
+            border = background.red;
         } else if ( hover.length > 1 && hover[1] === 'flip' ) {
             color = background.orange;
-        }
+            border = background.orange;
+        } 
         if ( hover.length === 1 && hover[0] === 'lava' ){
             border = background.red;
             color = dark2;
             radius = borderRadius.small;
-            borderWidth = 24
         }
         // console.log('Item'+count+', hover', hover)
         // console.log('Item'+count+', click', click)
@@ -124,7 +132,7 @@ export default function makeSquareArr ( startType, widthOfSq, inputMap, gameChec
             height: (widthOfSq - borderWidth)+'px',
             background: color,
             borderRadius: radius,
-            //border: `${borderWidth/2}px solid ${border}`,
+            border: `${borderWidth/2}px solid ${border}`,
         }
         let preStartStyle = {
             boxSizing: 'border-box',
@@ -132,9 +140,10 @@ export default function makeSquareArr ( startType, widthOfSq, inputMap, gameChec
             height: (widthOfSq - borderWidth)+'px',
             background: chroma(color).desaturate(2),
             borderRadius: radius,
-            //border: `${borderWidth/2}px solid ${border}`,
+            border: `${borderWidth/2}px solid ${chroma(border).desaturate(2)}`,
         }
         if ( startType === 'start' ){
+
             squaresArr.push(<div style={startStyle} data={key} key={count} onMouseEnter={(e) => onHover(e, key)} onClick={(e) => onClick(e, key)}></div>)
         } else if ( startType === 'pre-start' ){
             squaresArr.push(<div style={preStartStyle} data={key} key={count} ></div>)
